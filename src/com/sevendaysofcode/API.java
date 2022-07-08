3b338462d2bb6385784b8d474eaf3e54c5e9aa4d
aloneinabyss <thiago-assi@outlook.com>
2022-07-08 19:06:14 -0300

    http request and response

--- src/com/sevendaysofcode/API.java
+++ src/com/sevendaysofcode/API.java
@@ -1,14 +1,38 @@
 package com.sevendaysofcode;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public abstract class API {
 
-    public static void validateKey(String key) throws Exception {
+    public static String validateKey(String key) throws Exception {
         if (!key.startsWith("API_KEY=")) {
             throw new Exception("Missing API_KEY argument");
         }
 
-        if (key.split("=").length != 2) {
+        final String[] splittedKey = key.split("=");
+        if (splittedKey.length != 2) {
             throw new Exception("Invalid API_KEY format");
+        }
+
+        return splittedKey[1];
+    }
+
+    public static String connect(String url) throws Exception {
+        URI uri = URI.create(url);
+
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+
+        try {
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new Exception("Error connecting to API");
         }
     }
 
--- src/com/sevendaysofcode/App.java
+++ src/com/sevendaysofcode/App.java
@@ -2,14 +2,22 @@ package com.sevendaysofcode;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         if (args.length != 1) {
             throw new Exception("Missing API_KEY argument");
         }
 
-        API.validateKey(args[0]);
+        final String key = API.validateKey(args[0]);
 
         System.out.println("--------------------------------");
         System.out.println("        7 Days of Code          ");
         System.out.println("--------------------------------");
+
+        final String url = "https://imdb-api.com/en/API/Top250Movies/" + key;
+
+        final String resp = API.connect(url);
+
+        System.out.println(resp);
+
     }
 }
